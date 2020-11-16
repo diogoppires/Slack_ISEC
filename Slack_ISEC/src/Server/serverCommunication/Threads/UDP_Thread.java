b650662376f03cca5f4ef58a661553544e2e8982
Hugo Ferreira <gandamaluko379@gmail.com>
2020-11-16 00:34:56 +0000

    Started List of Free Servers

--- Slack_ISEC/src/Server/serverCommunication/Threads/UDP_Thread.java
+++ Slack_ISEC/src/Server/serverCommunication/Threads/UDP_Thread.java
@@ -3,8 +3,13 @@ package Server.serverCommunication.Threads;
 import Server.serverCommunication.Data.ServerInfo;
 import Server.serverCommunication.CommsTypes.MulticastCommunication;
 import Server.serverCommunication.CommsTypes.UDPCommunication;
+import Server.serverCommunication.Data.ServerData;
 import java.io.IOException;
 import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -26,15 +31,18 @@ public class UDP_Thread extends Thread {
         this.iS = infoS;
         this.exit = true;
     }
-
+    
     @Override
     public void run(){
         try {
             while(exit){
                 String msg = udpC.receiveUDP();
                 if(msg.equals(TCP_CONNECTION)){
+                    if (verifyCap(iS, udpC)){
                     iS.addClient(udpC.getServerPort());
-                    udpC.sendUDP("SUCCESS");
+                    udpC.sendUDP("SUCCESS"); 
+                    } else
+                        sendServersList(iS, udpC);
                     synchronized(iS.getAllServersData()){
                         iS.getAllServersData().get(udpC.getServerPort()).setPing(true);
                     }
@@ -47,4 +55,73 @@ public class UDP_Thread extends Thread {
             Logger.getLogger(UDP_Thread.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
+    
+   
+
+   
+     public boolean verifyCap(ServerInfo sI, UDPCommunication updC) {
+        
+       for (Map.Entry<Integer, ServerData> obj : iS.getAllServersData().entrySet()) {
+                synchronized (iS.getAllServersData()) {
+                    if (obj.getValue().getServerDetails().getnClients() < sI.getServerInfo(udpC.getServerPort()).getNClientsServer() / 2) {
+                        System.out.println("Este Servidor tem: " + sI.getServerInfo(udpC.getServerPort()).getNClientsServer() + " Clientes.");
+                        System.out.println("O Servidor: " + obj.getValue().getServerDetails().getPortServer() + " tem " + obj.getValue().getServerDetails().getnClients() + " Clientes");
+                        return false;
+                    }
+                }
+            }
+       return true;
+    }
+      public void sendServersList(ServerInfo iS, UDPCommunication udpC) {
+        
+        ArrayList<Server> serversList = new ArrayList<>();
+
+        for (Map.Entry<Integer, ServerData> obj : iS.getAllServersData().entrySet()) {
+                    synchronized (iS.getAllServersData()) {
+                        serversList.add(new Server(obj.getValue().getServerDetails().getIpServer(),
+                                obj.getValue().getServerDetails().getPortServer(),
+                                obj.getValue().getServerDetails().getnClients()));
+                    obj.getValue().getServerDetails().getIpServer();
+                    Collections.sort(serversList, new sortByClients()); 
+                }
+            }
+        
+         for (Server s : serversList){
+             
+             System.out.println(s.toString());
+         
+         }
+        
+    }
+    
 }
+
+class Server {
+    String ip;
+    int port;
+    int nClients;
+
+    public Server(String ip, int port, int nClients) {
+        this.ip = ip;
+        this.port = port;
+        this.nClients = nClients;
+    }
+    
+    @Override
+    public String toString(){
+        return "Server: " + ip +":"+ port + " Clientes " + nClients;
+    }
+}
+class sortByClients implements Comparator<Server> 
+{ 
+    // Used for sorting in ascending order of 
+    // roll number 
+    public int compare(Server a, Server b) 
+    { 
+        return a.nClients - b.nClients; 
+    } 
+} 
+    
+    
+
