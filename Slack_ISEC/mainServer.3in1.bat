4e56ad3f21d1ac0b5aecb22580eab17c5e2a0be9
Hugo Ferreira <gandamaluko379@gmail.com>
2020-11-16 17:38:47 +0000

    Add bath Files v2

--- Slack_ISEC/mainServer.3in1.bat
+++ Slack_ISEC/mainServer.3in1.bat
@@ -2,6 +2,4 @@ start cmd /k call mainServer9999.bat
 start cmd /k call mainServer9998.bat
 start cmd /k call mainServer9997.bat
 
-pause
-
 
--- Slack_ISEC/maniClient.bat
+++
@@ -1,5 +0,0 @@
-cd build\classes
-java Client.mainClient
-pause
-
-
